7c819071e07293c4c08cf277af4726c1c2e7e74c
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-03-19 16:20:50 +0500

    added product and user schema

--- Backend/config/mongoDb.js
+++ Backend/config/mongoDb.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("db connected");
-  });
-  await mongoose.connect(`${process.env.MONGODB_URL}/ecommerce-cloths`);
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("db connected");
+    });
+    await mongoose.connect(`${process.env.MONGODB_URL}/ecommerce-cloths`);
+  } catch (error) {
+    console.error("DB connection error", error);
+    process.exit(1);
+  }
 };
 
 export default connectDb;
---
+++ Backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cartData: {
+      type: Object,
+      default: {}, // initializes an empty object for the user cart data
+    },
+  },
+  { minimize: false }, // this handles empty objects when saving data, by default mongoose deletes empty objects but due to minimize it wont be deleted
+);
+
+const user = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default user;

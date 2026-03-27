9f21df43d69421b4fa510509d1e367c39b532e49
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-03-27 11:36:00 +0500

    added routes to add,list and order

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
@@ -9,6 +12,13 @@ const App = () => {
       <hr />
       <div className="flex w-full">
         <Sidebar />
+        <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600">
+          <Routes>
+            <Route path="/add" element={<Add />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );

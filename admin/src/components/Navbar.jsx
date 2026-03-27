9e6765b9129869b6b82fbcf670dd4ca6727a73d9
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-03-27 11:08:22 +0500

    added navbar

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img src={assets.logo} alt="" className="w-[max(10%,80px)]" />
+      <button className="px-5 py-2 text-white bg-gray-600 rounded-full sm:px-7 sm:text-sm">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;

a303567a4740d5996f48bb13d9ad0e987f8b94a6
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-03-27 11:27:18 +0500

    added the side bar and font

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
       <Navbar />
-    </>
+      <hr />
+      <div className="flex w-full">
+        <Sidebar />
+      </div>
+    </div>
   );
 };
 

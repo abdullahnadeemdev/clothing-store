63b06338ecf3b69b50a209d8d3b9f5b8b2fa347c
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-03-31 16:37:44 +0500

    created cart controllers

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -1,8 +1,68 @@
+import userModel from "../models/userModel";
+
 // add products to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+    const userData = await userModel.findById(userId);
 
-const updateCart = async (req, res) => {};
+    const cartData = await userData.cartData;
 
-const getUserCart = async (req, res) => {};
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+
+    res.json({ success: true, msg: "Added to cart" });
+  } catch (error) {
+    console.log(error.message);
+
+    res.json({ success: false, msg: error.message });
+  }
+};
+
+// Update cart login
+
+const updateCart = async (req, res) => {
+  try {
+    const { userId, itemId, size, quantity } = req.body;
+    const userData = await userModel.findById(userId);
+
+    const cartData = await userData.cartData;
+
+    cartData[itemId][size] = quantity;
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, msg: "Cart updated" });
+  } catch (error) {
+    console.log(error.message);
+
+    res.json({ success: false, msg: error.message });
+  }
+};
+
+// Get user cart logic
+const getUserCart = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const userData = await userModel.findById(userId);
+
+    const cartData = await userData.cartData;
+
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, msg: error.message });
+  }
+};
 
 export { addToCart, updateCart, getUserCart };
